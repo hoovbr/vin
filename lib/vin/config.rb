ab93e532637f5a1dcd6b8912e383d6d44378dca1
Roger Oba <rogerluan.oba@gmail.com>
2023-09-27 05:22:13 -0300

    Change behavior of default shard range to be 0..0.

--- lib/vin/config.rb
+++ lib/vin/config.rb
@@ -82,8 +82,8 @@ class VIN
 
     def fetch_allowed_range!
       range = Range.new(
-        ENV.fetch("VIN_LOGICAL_SHARD_ID_RANGE_MIN", logical_shard_id_allowed_range.min).to_i,
-        ENV.fetch("VIN_LOGICAL_SHARD_ID_RANGE_MAX", logical_shard_id_allowed_range.max).to_i,
+        ENV.fetch("VIN_LOGICAL_SHARD_ID_RANGE_MIN", 0).to_i,
+        ENV.fetch("VIN_LOGICAL_SHARD_ID_RANGE_MAX", 0).to_i,
       )
       unless (logical_shard_id_allowed_range.to_a & range.to_a) == range.to_a
         raise ArgumentError, "VIN_LOGICAL_SHARD_ID_RANGE_MIN and VIN_LOGICAL_SHARD_ID_RANGE_MAX env vars compose a range outside the allowed range of #{logical_shard_id_allowed_range} defined by the number of bits in VIN_LOGICAL_SHARD_ID_BITS env var."
